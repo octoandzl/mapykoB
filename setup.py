3f52d864bb7c34367ec581c9eb826af2a8a4a008
Aadil <andzlhub@gmail.com>
2020-08-28 18:23:36 +0200

    Set up setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,24 @@
 
 
 from distutils.core import setup
+with open("README.rst") as file_object:
+    description = file_object.read()
 
 setup(name='mapykoB',
       version='0.0.2',
-      description="Package for sequence modeling with Andrei Markov's models",
       author='Andzl',
       author_email='andzlhub@gmail.com',
       url='https://github.com/octoandzl/mapykoB',
+      long_description=description,
+      description="Package for sequence modeling with Andrei Markov's models",
+      license="MIT",
       packages=['mapykoB', 'mapykoB.discreet'],
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    data_files=[("", ["LICENSE"])]
       )
