46f235c2071d8505cccfedb99b4c5438908bd773
Aadil <andzlhub@gmail.com>
2020-08-28 11:41:55 +0200

    bug fixed

--- mapykoB/__init__.py
+++ mapykoB/__init__.py
@@ -8,4 +8,4 @@
 from .discreet.hmm_discreet import hmm_discreet
 from . generate_data import generate_sequence
 from . generate_data import generate_sequences
-from . generated_data import generate_random_sequences
+from . generate_data import generate_random_sequences
